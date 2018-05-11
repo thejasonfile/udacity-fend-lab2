41f4425cd6bcced336a0b6c503f17dd1920ff375
Jason Arnold <jason.arnold.00@gmail.com>
2018-05-11 15:48:18 -0400

    Add nested loops to makeGrid() to build grid

--- designs.js
+++ designs.js
@@ -1,14 +1,32 @@
 // Select color input
-const colorInput = document.querySelector('#colorPicker');
+const colorInput = document.querySelector("#colorPicker");
 // Select size input
-const inputHeight = document.querySelector('#inputHeight');
-const inputWidth = document.querySelector('#inputWidth');
+const inputHeight = document.querySelector("#inputHeight");
+const inputWidth = document.querySelector("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
 const submitButton = document.querySelector("input[type='submit']");
+submitButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  makeGrid()
+});
 
 function makeGrid() {
   // Your code goes here!
-  console.log(inputHeight.value, inputWidth.value);
-
+  const gridParent = document.querySelector("#pixelCanvas");
+  //clear existing Grid
+  
+  //make new Grid
+  for (let i = inputHeight.value; i > 0; i--) {
+    const newRow = document.createElement('tr');
+    gridParent.appendChild(newRow);
+    for (let j = inputWidth.value; j > 0; j--) {
+      const currentRow = gridParent.lastChild;
+      const newData = document.createElement('td');
+      currentRow.appendChild(newData);
+    }
+  }
 }
+
+//clear existing grid
+//add eventlisteners to cells to add color
