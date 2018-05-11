36c3e9da5f8e9884c18ab70eb4275e0f841ad192
Jason Arnold <jason.arnold.00@gmail.com>
2018-05-11 15:17:37 -0400

    Add variables for inputs

--- designs.js
+++ designs.js
@@ -1,8 +1,14 @@
 // Select color input
+const colorInput = document.querySelector('#colorPicker');
 // Select size input
+const inputHeight = document.querySelector('#inputHeight');
+const inputWidth = document.querySelector('#inputWidth');
 
 // When size is submitted by the user, call makeGrid()
+const submitButton = document.querySelector("input[type='submit']");
 
 function makeGrid() {
   // Your code goes here!
+  console.log(inputHeight.value, inputWidth.value);
+
 }
